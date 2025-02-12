f4ed8a8c1dae7736cf859de2da49abe6696c4049
Verena Ankerl <ankerl.verena@gmail.com>
2025-02-12 10:33:58 +0000

    build sales-margin model

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,10 @@
-select *
+select 
+    date_date
+    , orders_id
+    , p.products_id
+    , revenue
+    , quantity
+    , purchase_price
     , quantity * purchase_price AS purchase_cost
     , ROUND(revenue - (quantity * purchase_price), 2) AS margin
 from {{ ref("stg_raw__sales") }} s
